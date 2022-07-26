e78b4c459a084b25ee22c2e7f3cf6c634907b996
Patryk <ricochet852@gmail.com>
2022-07-26 20:37:25 +0200

    Dodanie serwisu do authentykacji uztywkonikow

---
+++ ShopList/Shared/DataModels/Authentication/IdentityUserModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShopList.Shared.DataModels.Authentication
+{
+  public class IdentityUserModel : IdentityUser
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+  }
+}
