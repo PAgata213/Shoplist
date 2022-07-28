743aec6242019db4b0eaff3d108d20eb392f9e25
Patryk <ricochet852@gmail.com>
2022-07-28 18:01:49 +0200

    Dodanie modeli do authentykacji

---
+++ ShopList/Shared/DataModels/Authentication/AuthenticatedUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Shared.DataModels.Authentication
+{
+  public class AuthenticatedUserModel
+  {
+    public string AccessToken { get; set; }
+    public string UserName { get; set; }
+    public string ErrorMessage { get; set; }
+  }
+}
