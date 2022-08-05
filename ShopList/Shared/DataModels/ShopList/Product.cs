e3330fd48b5a9bc68ccfe7c6231ff7ea81db946e
Patryk <ricochet852@gmail.com>
2022-08-05 13:12:53 +0200

    Dodanie Tabeli Produktow

---
+++ ShopList/Shared/DataModels/ShopList/Product.cs
@@ -0,0 +1,18 @@
+﻿using ShopList.Shared.DataModels.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Shared.DataModels.ShopList
+{
+  public class Product : EntityModel
+  {
+    [Required]
+    public string ProductName { get; set; }
+
+    public string? Description { get; set; }
+  }
+}
