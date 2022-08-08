b884434cd6728b34fe23a8e88c9cc538d4b72f38
Patryk <ricochet852@gmail.com>
2022-08-08 12:19:38 +0200

    Db Migration

--- ShopList.DataAccess/Migrations/20220808095928_RestrictRemoveFromDataBase.Designer.cs
+++ ShopList.DataAccess/Migrations/20220808095928_RestrictRemoveFromDataBase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopList.DataAccess.DataContexts;
 
@@ -11,9 +12,11 @@ using ShopList.DataAccess.DataContexts;
 namespace ShopList.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220808095928_RestrictRemoveFromDataBase")]
+    partial class RestrictRemoveFromDataBase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShopList.DataAccess/Migrations/20220808095928_RestrictRemoveFromDataBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopList.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestrictRemoveFromDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shops_ShopBrands_ShopBrandId",
+                table: "Shops");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shops_ShopBrands_ShopBrandId",
+                table: "Shops",
+                column: "ShopBrandId",
+                principalTable: "ShopBrands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shops_ShopBrands_ShopBrandId",
+                table: "Shops");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shops_ShopBrands_ShopBrandId",
+                table: "Shops",
+                column: "ShopBrandId",
+                principalTable: "ShopBrands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
