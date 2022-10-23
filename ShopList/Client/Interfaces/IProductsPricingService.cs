b2f9c7bcffd39790d0ac7d644ed0729db09e9333
Patryk <ricochet852@gmail.com>
2022-10-23 18:27:54 +0200

    Product pricing Service

---
+++ ShopList/Client/Interfaces/IProductsPricingService.cs
@@ -0,0 +1,12 @@
+﻿using ShopList.Shared.DataModels.DTOs;
+
+namespace ShopList.Client.Interfaces
+{
+    public interface IProductsPricingService
+    {
+        Task<IEnumerable<ProductPricingDTO>> GetProductsPricingAsync();
+        Task<ProductPricingDTO> CreateProductPricingAsync(ProductPricingDTO productPricingDTO);
+        Task<ProductPricingDTO> UpdateProductPricingAsync(ProductPricingDTO productPricingDTO);
+        Task<bool> RemoveProductPricingAsync(ProductPricingDTO productPricingDTO);
+    }
+}
---
+++ ShopList/Client/Services/ProductsPricingService.cs
@@ -0,0 +1,50 @@
+﻿using ShopList.Client.Interfaces;
+using ShopList.Shared.DataModels.DTOs;
+using ShopList.Shared.DataModels.ShopList;
+
+namespace ShopList.Client.Services
+{
+    public class ProductsPricingService : IProductsPricingService
+	{
+		private readonly IAPIHelper _apiHelper;
+
+		public ProductsPricingService(IAPIHelper apiHelper)
+		{
+			_apiHelper = apiHelper;
+		}
+
+		public async Task<IEnumerable<ProductPricingDTO>> GetProductsPricingAsync()
+		{
+			var content = await _apiHelper.GetAsync<IEnumerable<ProductPricingDTO>>(ShopList.Shared.APIAdressess.GetProductsPricing);
+			return content.DataModel ?? Enumerable.Empty<ProductPricingDTO>();
+		}
+
+		public async Task<ProductPricingDTO> CreateProductPricingAsync(ProductPricingDTO productPricingDTO)
+		{
+			var result = await _apiHelper.PostAsync<ProductPricingDTO, ProductPricingDTO>(ShopList.Shared.APIAdressess.CreateProductPricing, productPricingDTO);
+			if(!result.IsSuccessStatusCode)
+			{
+				throw new Exception(result?.ErrorMessage ?? result!.StatusCode.ToString());
+			}
+
+			return result!.DataModel!;
+		}
+
+		public async Task<ProductPricingDTO> UpdateProductPricingAsync(ProductPricingDTO productPricingDTO)
+		{
+			var result = await _apiHelper.PutAsync<ProductPricingDTO, ProductPricingDTO>(ShopList.Shared.APIAdressess.UpdateProductPricing, productPricingDTO);
+			if(!result.IsSuccessStatusCode)
+			{
+				throw new Exception(result?.ErrorMessage ?? result!.StatusCode.ToString());
+			}
+
+			return result!.DataModel!;
+		}
+
+		public async Task<bool> RemoveProductPricingAsync(ProductPricingDTO productPricingDTO)
+		{
+			var result = await _apiHelper.PostAsync<ProductPricingDTO, ProductPricingDTO>(ShopList.Shared.APIAdressess.RemoveProductPricing, productPricingDTO);
+			return result.IsSuccessStatusCode;
+		}
+	}
+}
