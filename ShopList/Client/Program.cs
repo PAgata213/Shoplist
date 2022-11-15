345dd47e8075c7dee8a47d23800349496fc00881
Patryk <ricochet852@gmail.com>
2022-11-15 20:20:50 +0100

    - Toast

--- ShopList/Client/Program.cs
+++ ShopList/Client/Program.cs
@@ -6,12 +6,16 @@ using ShopList.Client.Interfaces;
 using ShopList.Client.Services;
 using System.Reflection;
 using AutoMapper;
+using Blazored.Toast;
+using Blazored.Modal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAutoMapper(typeof(ShopList.Shared.Helpers.MapperProfile).GetTypeInfo().Assembly);
+builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredToast();
 
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IShopBrandsService, ShopBrandsService>();
